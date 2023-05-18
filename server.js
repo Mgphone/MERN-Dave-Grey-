67ccd988ca221d84ef9cccb5efec8cc0812d66e7
Mgphone <51868037+Mgphone@users.noreply.github.com>
2023-05-18 04:41:10 +0100

    lesson 3

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
+require ('dotenv').config()
 const express=require ("express")
 const app=express()
 const path=require('path')
-const{logger}=require('./middleware/logger')
+const{logger,logEvents }=require('./middleware/logger')
 const errorHandler=require('./middleware/errorHandler')
 const cookieParser=require('cookie-parser')
 const cors=require('cors')
 const corsOptions=require('./config/corsOptions')
-
+const connectDb=require('./config/dbConn')
+const { default: mongoose } = require('mongoose')
+const { log } = require('console')
 const PORT=process.env.PORT||3000
 
+connectDb();
 
 app.use(logger)
 app.use(express.json());
@@ -21,6 +25,16 @@ app.use('/',require('./routes/root'))
 
 
 app.use(errorHandler)
-app.listen(PORT,()=>{
-  console.log(`Server is listening on port ${PORT}`)
+
+mongoose.connection.once('open',()=>{
+  console.log('Connect to the Mongodb');
+  app.listen(PORT,()=>{
+    console.log(`Server is listening on port ${PORT}`)
+  })
 })
+
+mongoose.connection.on('error',err=>{
+  console.log(err);
+  logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
+
